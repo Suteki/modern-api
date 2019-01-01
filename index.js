d283ae2b9c32e5bd7ddb53ea9ae3b5a5cfa82e95
Sergey Borachuk <sergeyborachuk@gmail.com>
2019-01-01 02:09:27 +0200

    connect to mongodb on mlab.com

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const hapi = require('hapi')
+const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://<user>:<password>@<dsyourds>.mlab.com:port/<database-name>')
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds125362.mlab.com:25362/modern_api')
+mongoose.connect('mongodb://testuser:test1Password@ds125362.mlab.com:25362/modern_api')
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database')
+})
 
 const server = hapi.server({
   port: 8000,
